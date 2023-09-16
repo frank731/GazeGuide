f3a368b263e6c21908e8962133d6a8143ed8f436
frank731 <bobbyflip332@gmail.com>
2023-09-16 17:04:18 -0400

    Set up calibration

--- simple/simple_example.py
+++ simple/simple_example.py
@@ -19,23 +19,8 @@ set_rot = []
 last_good_gaze = [0, 0]
 last_good_rot = []
 eye_gaze_factor = 30
-x_angle_thresh = 25
-y_angle_thresh = 20
-PORT = 1000
-IP = "127.0.0.1"
-DATA_RECEIVED = False
-
-class UDPProtocol:
-    def __init__(self):
-        pass
-
-    def connection_made(self, transport):
-        self.transport = transport
-
-    def datagram_received(self, data, addr):
-        global DATA_RECEIVED
-        DATA_RECEIVED = True
-        print("hellooojijoijoij")
+x_angle_thresh = 20
+y_angle_thresh = 15
 
 class EulerRotationOrder(enum.IntEnum):
     '''Various Euler rotation orders. lowercase x,y, z stand for the axes of the world coordinate system, and
@@ -62,22 +47,23 @@ def vector_to_angles(xpos, ypos, zpos, rotation_order: EulerRotationOrder = Eule
     return [azimuth, elevation]
 
 def check_look_away():
-    eye_az_diff = cur_gaze[0] - set_gaze[0]
-    eye_el_diff = cur_gaze[1] - set_gaze[1]
-    head_roll_diff = cur_rot[0] - set_rot[0] # Tilting head kind of affects position so eyes should compensate a little 
-    head_az_diff = cur_rot[1] - set_rot[1]
-    head_el_diff = cur_rot[2] - set_rot[2]
-    az_diff = eye_az_diff + head_az_diff
-    el_diff = eye_el_diff + head_el_diff
-    #print(cur_gaze, set_gaze, cur_rot, set_rot, az_diff, el_diff)
-    if az_diff >= x_angle_thresh:
-        print("Too left")
-    elif az_diff <= -x_angle_thresh:
-        print("Too right")
-    if el_diff >= y_angle_thresh:
-        print("Too up")
-    elif el_diff <= -y_angle_thresh:
-        print("Too down")
+    if len(set_gaze) > 0:
+        eye_az_diff = cur_gaze[0] - set_gaze[0]
+        eye_el_diff = cur_gaze[1] - set_gaze[1]
+        head_roll_diff = cur_rot[0] - set_rot[0] # Tilting head kind of affects position so eyes should compensate a little 
+        head_az_diff = cur_rot[1] - set_rot[1]
+        head_el_diff = cur_rot[2] - set_rot[2]
+        az_diff = eye_az_diff + head_az_diff
+        el_diff = eye_el_diff + head_el_diff
+        print(az_diff, el_diff)
+        if az_diff >= x_angle_thresh:
+            print("Too right")
+        elif az_diff <= -x_angle_thresh:
+            print("Too left")
+        if el_diff >= y_angle_thresh:
+            print("Too up")
+        elif el_diff <= -y_angle_thresh:
+            print("Too down")
 class FrontendData:
     ''' BLE Frontend '''    
     def __init__(self):
@@ -109,6 +95,7 @@ class FrontendData:
     #TODO Test angle directions, see if eyes move a lot or stay off
     def _handle_et_data(et_data: adhawkapi.EyeTrackingStreamData):
         global cur_gaze, cur_rot, set_gaze, set_rot, eye_gaze_factor, last_good_gaze, last_good_rot
+        #print("running")
         ''' Handles the latest et data '''
         if et_data.gaze is not None:
             cur_gaze = vector_to_angles(et_data.gaze[0], et_data.gaze[1], et_data.gaze[2])
@@ -117,10 +104,8 @@ class FrontendData:
                 cur_gaze = last_good_gaze
             else:
                 last_good_gaze = cur_gaze
-            if len(set_gaze) == 0:
-                set_gaze = cur_gaze
-            print("Gaze: ")
-            print(cur_gaze)
+            #print("Gaze: ")
+            #print(cur_gaze)
             #xvec, yvec, zvec, vergence = et_data.gaze
             #print(f'Gaze={xvec:.2f},y={yvec:.2f},z={zvec:.2f},vergence={vergence:.2f}')
 
@@ -128,10 +113,8 @@ class FrontendData:
             if et_data.eye_mask == adhawkapi.EyeMask.BINOCULAR:
                 cur_rot = R.from_quat(et_data.imu_quaternion).as_euler('zyx', degrees=True)
                 #(cur_rol, cur_az, cur_el) = 
-                if len(set_rot) == 0:
-                    set_rot = cur_rot
-                print("Rotation: ")
-                print(cur_rot)
+                #print("Rotation: ")
+                #print(cur_rot)
                 #x, y, z, w = et_data.imu_quaternion
                 #print(f'IMU: roll={cur_rol:.2f},azimuth={cur_az:.2f},elevation={cur_el:.2f}')
         check_look_away()
@@ -150,7 +133,7 @@ class FrontendData:
 
     def _handle_tracker_connect(self):
         print("Tracker connected")
-        self._api.set_et_stream_rate(2, callback=lambda *args: None)
+        self._api.set_et_stream_rate(30, callback=lambda *args: None)
 
         self._api.set_et_stream_control([
             adhawkapi.EyeTrackingStreamTypes.GAZE,
@@ -167,23 +150,23 @@ class FrontendData:
 
 
 async def main():
+    global set_gaze, set_rot
     ''' App entrypoint '''
     frontend = FrontendData()
 
-    print("Starting UDP server")
-
-    # Get a reference to the event loop as we plan to use
-    # low-level APIs.
-    loop = asyncio.get_running_loop()
-
-    udp_endpoint = await asyncio.start_datagram_endpoint(
-        lambda: UDPProtocol(),  # Use a simple protocol
-        local_addr=(IP, PORT)
-    )
-
+    # Create a socket to listen for the signal
+    receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    receiver_socket.bind(("127.0.0.1", 12345))  # Bind to the same address as the sender
+    receiver_socket.listen(1) 
     try:
         while True:
-            time.sleep(1)
+            conn, addr = receiver_socket.accept()  # Accept a connection
+            data = conn.recv(1024)  # Receive data from the sender
+            if data == b"button_pressed":
+                set_gaze = cur_gaze
+                set_rot = cur_rot
+                print(cur_gaze, cur_rot)
+            time.sleep(0.5)
     except (KeyboardInterrupt, SystemExit):
         frontend.shutdown()
 
--- usr_interface/gui.py
+++ usr_interface/gui.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import socket 
+import asyncio
 
+#RUNT HIS FIRST
 UDP_IP = "127.0.0.1"
 UDP_PORT = 1000
 MESSAGE = "REQUEST LOCATION"
@@ -16,7 +18,7 @@ class Window(Frame):
 
         # create button, link it to clickExitButton()
         exitButton = Button(self, text="Send Signal", command=self.send_signal)
-
+        
         # place button at (0,0)
         exitButton.place(x=0, y=0)
 
@@ -24,10 +26,11 @@ class Window(Frame):
         exit()
 
     def send_signal(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
-        sock.sendto(bytes(MESSAGE, "utf-8"), (UDP_IP, UDP_PORT))
+        sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sender_socket.connect(("127.0.0.1", 12345))
+        sender_socket.send(b"button_pressed")
 
-        
+
 root = Tk()
 app = Window(root)
 root.wm_title("Tkinter button")
